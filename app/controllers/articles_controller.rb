9255b38443a9e392e8873d43b0ca4614f711e72b
david vermeulen <87286155+kindadumbdave@users.noreply.github.com>
2023-03-31 18:39:55 +0200

    return your library books

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
        else
         render 'new'        
@@ -26,10 +26,10 @@ end
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
-            flash[:notice] = "Article has been updated"
+            flash[:success] = "Article has been updated"
             redirect_to article_path(@article)
         else
-            flash[:notice] = "Article not updated"
+            flash[:success] = "Article not updated"
             render 'edit'
         end
     end    
@@ -42,7 +42,7 @@ end
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        flash[:notice] = "Article was deleted"
+        flash[:success] = "Article was deleted"
         redirect_to articles_path
     end
 
